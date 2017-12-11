dad2134b1924c765184f65f89e1d2a32d20f9d12
hannah fresques <fresques@users.noreply.github.com>
2017-12-11 07:52:11 -0500

    more comments for R script

--- 2017fall/inspections/healthInspections.r
+++ 2017fall/inspections/healthInspections.r
@@ -10,7 +10,9 @@ library(dplyr)
 library(readr)
 
 
-# read data from api
+
+### Read data from api ###
+# set the limit way above the expected size of the database, to get all the data.
 insp <- read_csv(url('https://data.cityofnewyork.us/resource/9w7m-hzhe.csv?$limit=1000000'))
 
 # check problems importing
@@ -19,6 +21,9 @@ problems(insp) %>% select(col) %>% table(useNA = "always")
 # only problems were with the zip code column. 
 # I don't care about that column so I'll just plow ahead.
 
+
+
+### Look at the data ###
 insp %>% head(100) %>% View()
 
 insp %>% colnames()
@@ -32,15 +37,19 @@ insp %>% select(camis, inspection_date) %>% unique() %>% nrow()
 insp %>% select(camis, inspection_date,violation_code) %>% unique() %>% nrow()
 # that combination almost uniquely idetifies rows, but not quite.
 
+
+
+### Filter data ###
+
 # we're only interested in grades, not each violation, so cut down the data
 insp2 <- insp %>% select(camis,dba,boro,cuisine_description,grade,grade_date) %>% unique()
-
 
 # only keep the latest grade for each restaurant.
 insp3 <- insp2 %>% 
   group_by(camis) %>%
   slice(which.max(grade_date)) %>%
   as.data.frame()
+# check that expected number of rows was kept.
 insp %>% filter(is.na(grade_date)==F) %>% select(camis, dba) %>% unique() %>% nrow()
 insp3 %>% nrow()
 # yes, expected numbers of rows were kept.
@@ -48,15 +57,20 @@ insp3 %>% nrow()
 # what dates are covered?
 insp3 %>% select(grade_date) %>% summary() 
 
-# for simplcity, limit to restaurants inspected in 2017.
+# Limit to restaurants inspected in 2017.
 insp4 <- insp3 %>%
   filter(grade_date >= as.POSIXlt("2017/1/1 00:00:00") & boro != "Missing") %>%
   rename(cuisine=cuisine_description)
 insp4 %>% select(grade_date) %>% summary() 
+# All are in 2017. great.
 
-# great.
+# Look at grades assgined
 insp4 %>% select(grade) %>% table()
 # per data dictionary, Z means grade pending.
+
+
+
+### Summarize the data ###
 
 # summarize by boro
 boro <- insp4 %>%
@@ -84,7 +98,9 @@ cuis <- insp4 %>%
 cuis %>% View()
 cuis %>% summary()
 
-# save files
+
+
+### Save files ###
 # there must be a get the location of a script but I haven't found it.
 # rootDir <- "yourFilePathHere"
 write.csv(boro, paste0(rootDir,"boro.csv"), row.names = FALSE)
